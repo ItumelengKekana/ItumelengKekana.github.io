58e0f0696c3add3b43cac8d7b131f5cc27c37144
Itumeleng Kekana <itumeleng@letswhoosh.com>
2022-05-09 13:16:01 +0200

    Changed the home path from /portfolio to /

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const ejsMate = require('ejs-mate');
+const path = require("path");
+const ejsMate = require("ejs-mate");
 const port = process.env.PORT || 5000;
 
-const portfolioRoutes = require('./routes/portfolio');
+const portfolioRoutes = require("./routes/portfolio");
 
-app.engine('ejs', ejsMate);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/portfolio', portfolioRoutes);
-app.use('*', (req, res) => {res.render('main/404')});
+app.use("/", portfolioRoutes);
+app.use("*", (req, res) => {
+	res.render("main/404");
+});
 
-app.listen(port, () =>{
-    console.log('Server up!')
+app.listen(port, () => {
+	console.log("Server up!");
 });
